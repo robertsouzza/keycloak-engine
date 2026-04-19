db34e3f10dae155b3a2220ecd2a80493627e11b0
robertsouzza <rcsouzza@gmail.com>
2026-04-19 16:24:05 -0400

    feat: inicialização do esqueleto do projeto via Spring Initializr

---
+++ src/main/java/com/nano/keycloak_engine/KeycloakEngineApplication.java
@@ -0,0 +1,13 @@
+package com.nano.keycloak_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nano/keycloak_engine/KeycloakEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nano.keycloak_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
