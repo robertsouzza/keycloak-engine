507bcb76aa3d6ccfd378fdeba4171c70651672bb
robertsouzza <rcsouzza@gmail.com>
2026-04-21 15:36:36 -0400

    feat: implement user registration with Swagger docs and email verification

--- src/main/java/com/nano/keycloak_engine/controller/UserRegistrationController.java
+++ src/main/java/com/nano/keycloak_engine/controller/UserRegistrationController.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 
 import com.nano.keycloak_engine.service.KeycloakService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 /**
  * api REST CONTROLLER - responsável em pedir a criação de usuários. 
  */
@@ -13,10 +16,12 @@ import com.nano.keycloak_engine.service.KeycloakService;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Tag(name = "Usuários", description = "Endpoints para gestão de usuários") // Swagger Tag
 public class UserRegistrationController {
 
     private final KeycloakService keycloakService;
 
+    @Operation(summary = "Criar novo usuário", description = "Registra um usuário no Keycloak e atribui a USER_ROLE")
     @PostMapping("/create")
     public String register(@RequestBody UserRequest request) {
         keycloakService.criarUsuario(
